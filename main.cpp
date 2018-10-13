7e9f84ec4fc5f33851bc68a8a7038764cc9a6dc9
Tyler <you@example.com>
2018-10-13 18:33:17 -0500

    Adding base app

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+ cout<<"Enter Date Command"<<endl;
+ string input;
+ getline (cin, input);
+ cout<<input<<endl;
+
+
+ return 0;
+
+}
